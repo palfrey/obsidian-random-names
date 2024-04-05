5d5c6af5ded267af82f445f5984cef7a75fd0239
Tom Parker-Shemilt <palfrey@tevp.net>
2024-04-05 18:46:59 +0100

    Lots of lint/reformatting

--- main.ts
+++ main.ts
@@ -1,17 +1,19 @@
-import { Editor, MarkdownView, Plugin } from 'obsidian';
-import {choices} from './allgenerators'
+import { Editor, MarkdownView, Plugin } from "obsidian";
+import { choices } from "./allgenerators";
 import * as _ from "lodash";
 
 function toTitleCase(str: string): string {
-	return str.replace(
-	  /\w\S*/g,
-	  function(txt) {
+	return str.replace(/\w\S*/g, function (txt) {
 		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-	  }
-	);
+	});
 }
 
-const specialCases: { [key: string] : string } = {"xMens": "X-men", "sciFiGuns": "Sci-fi guns", "twentiethCenturyEnglishs": "20th Century English", "englishs": "English"};
+const specialCases: { [key: string]: string } = {
+	xMens: "X-men",
+	sciFiGuns: "Sci-fi guns",
+	twentiethCenturyEnglishs: "20th Century English",
+	englishs: "English",
+};
 
 function toPretty(str: string): string {
 	if (_.has(specialCases, str)) {
@@ -21,7 +23,9 @@ function toPretty(str: string): string {
 		return str.split("_").map(toPretty).join(" ");
 	} else if (str.slice(1).toLowerCase() != str.slice(1)) {
 		const num = str.slice(1).search(/[A-Z]/);
-		return [str.slice(0, num+1), str.slice(num+1)].map(toPretty).join(" ");
+		return [str.slice(0, num + 1), str.slice(num + 1)]
+			.map(toPretty)
+			.join(" ");
 	} else {
 		return toTitleCase(str);
 	}
@@ -29,18 +33,26 @@ function toPretty(str: string): string {
 
 export default class RandomNames extends Plugin {
 	async onload() {
-		_.keys(choices).sort().forEach(first => {
-			_.keys(choices[first]).sort().forEach(second => {
-				this.addCommand({
-					id: `random-names-${first}-${second}`,
-					name: `Insert name (${toPretty(first)} - ${toPretty(second)})`,
-					editorCallback: (editor: Editor, view: MarkdownView) => {
-						editor.replaceRange(
-							choices[first][second](),
-							editor.getCursor()
-						);				
-					}
-				});
-			});});
+		_.keys(choices)
+			.sort()
+			.forEach((first) => {
+				_.keys(choices[first])
+					.sort()
+					.forEach((second) => {
+						this.addCommand({
+							id: `random-names-${first}-${second}`,
+							name: `Insert name (${toPretty(first)} - ${toPretty(second)})`,
+							editorCallback: (
+								editor: Editor,
+								view: MarkdownView,
+							) => {
+								editor.replaceRange(
+									choices[first][second](),
+									editor.getCursor(),
+								);
+							},
+						});
+					});
+			});
 	}
 }
